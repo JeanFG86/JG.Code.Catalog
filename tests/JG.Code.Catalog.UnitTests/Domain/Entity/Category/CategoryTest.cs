249350024de291b874c8c740eada4876d52cb513
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-05-17 18:57:20 -0300

    Add project files.

---
+++ tests/JG.Code.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG.Code.Catalog.UnitTests.Domain.Entity.Category;
+public class CategoryTest
+{
+}
