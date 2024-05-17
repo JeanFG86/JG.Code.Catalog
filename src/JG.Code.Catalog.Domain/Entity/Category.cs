d4c6381be0c58357d2a3d9a110bce69403cfbc26
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-05-17 20:25:03 -0300

    create test Instantiate Category

---
+++ src/JG.Code.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace JG.Code.Catalog.Domain.Entity;
+public class Category
+{
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+
+    public Category(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }    
+}
--- tests/JG.Code.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/JG.Code.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JG.Code.Catalog.UnitTests.Domain.Entity.Category;
+﻿namespace JG.Code.Catalog.UnitTests.Domain.Entity.Category;
+using DomainEntity = JG.Code.Catalog.Domain.Entity;
 public class CategoryTest
 {
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggegates")]
+    public void Instantiate()
+    {
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description"
+        };
+
+        var category = new DomainEntity.Category(validData.Name, validData.Description);
+
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+    }
 }
