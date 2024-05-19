36ef5542588a4863f9c32f7ab521c0ca1575f529
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-05-19 15:36:52 -0300

    create Update test Category

--- tests/JG.Code.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/JG.Code.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -146,4 +146,17 @@ public class CategoryTest
 
         Assert.False(category.IsActive);
     }
+
+    [Fact(DisplayName = nameof(Update))]
+    [Trait("Domain", "Category - Aggegates")]
+    public void Update()
+    {
+        var category = new DomainEntity.Category("Category name", "Category description");
+        var newValues = new { Name = "New Name", Description = "New Description" };
+
+        category.Update(newValues.Name, newValues.Description);
+
+        Assert.Equal(newValues.Name, category.Name);
+        Assert.Equal(newValues.Description, category.Description);
+    }
 }
