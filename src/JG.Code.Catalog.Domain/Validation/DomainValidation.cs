274159aebfba37046c4806cc85381c743fc8afaf
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-06-16 17:59:14 -0300

    GetValuesSmallerThanMin test

--- src/JG.Code.Catalog.Domain/Validation/DomainValidation.cs
+++ src/JG.Code.Catalog.Domain/Validation/DomainValidation.cs
@@ -13,4 +13,12 @@ public class DomainValidation
         if (String.IsNullOrWhiteSpace(target as string))
             throw new EntityValidationException($"{fieldName} should not be null or empty");
     }
+
+    public static void MinLength(string target, int minLength, string fieldName)
+    {
+        if(target.Length < minLength)
+        {
+            throw new EntityValidationException($"{fieldName} should not be less than {minLength} characteres long");
+        }
+    }
 }
