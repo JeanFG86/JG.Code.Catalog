9ed2e116c70c5e771e00ba77bfa2b3754826df4d
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-07-01 17:04:03 -0300

    create GetCategoryInputValidator

---
+++ tests/JG.Code.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using JG.Code.Catalog.Application.UseCases.Category.GetCategory;
+
+namespace JG.Code.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryFixture))]
+public class GetCategoryInputValidatorTest
+{
+    private readonly GetCategoryFixture _fixture;
+
+    public GetCategoryInputValidatorTest(GetCategoryFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(ValidationOk))]
+    [Trait("Application", "GetCategoryInputValidation - Use Cases")]
+    public void ValidationOk()
+    {
+        var validInput = new GetCategoryInput(Guid.NewGuid());
+        var validator = new GetCategoryInputValidator();
+
+        var validationResult = validator.Validate(validInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeTrue();
+        validationResult.Errors.Should().HaveCount(0);
+    }
+}
