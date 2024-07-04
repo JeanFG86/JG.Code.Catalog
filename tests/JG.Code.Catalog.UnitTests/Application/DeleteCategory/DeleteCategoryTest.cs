a549d41d88f1321658c36d73928521472f8e08d5
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-07-03 22:03:12 -0300

    ThrowWhenCategoryNotFound test

--- tests/JG.Code.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
+++ tests/JG.Code.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
@@ -1,4 +1,7 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using JG.Code.Catalog.Application.Exceptions;
+using JG.Code.Catalog.Domain.Entity;
+using Moq;
 using UsesCases = JG.Code.Catalog.Application.UseCases.Category.DeleteCategory;
 
 namespace JG.Code.Catalog.UnitTests.Application.DeleteCategory;
@@ -29,5 +32,25 @@ public class DeleteCategoryTest
         repositoryMock.Verify(repository => repository.Get(categoryExample.Id, It.IsAny<CancellationToken>()), Times.Once);
         repositoryMock.Verify(repository => repository.Delete(categoryExample, It.IsAny<CancellationToken>()), Times.Once);
         unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+
+    [Fact(DisplayName = nameof(ThrowWhenCategoryNotFound))]
+    [Trait("Application", "DeleteCategory - Use Cases")]
+    public async Task ThrowWhenCategoryNotFound()
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var exampleGuid = Guid.NewGuid();
+        repositoryMock.Setup(x => x.Get(exampleGuid, It.IsAny<CancellationToken>())).ThrowsAsync(new NotFoundException($"Category '{exampleGuid}' not found"));
+        var input = new UsesCases.DeleteCategoryInput(exampleGuid);
+        var useCase = new UsesCases.DeleteCategory(repositoryMock.Object, unitOfWorkMock.Object);
+
+        var task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<NotFoundException>();
+        repositoryMock.Verify(repository => repository.Get(exampleGuid, It.IsAny<CancellationToken>()), Times.Once);
+        repositoryMock.Verify(repository => repository.Delete(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Never);
+        unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Never);
     }
 }
