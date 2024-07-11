611c38826f99ee6dc16c78390aea630a9a5ce108
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-07-11 17:49:20 -0300

    ListInputWithoutParameters test

--- tests/JG.Code.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
+++ tests/JG.Code.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -1,5 +1,7 @@
-﻿using JG.Code.Catalog.Domain.Entity;
+﻿using JG.Code.Catalog.Application.UseCases.Category.ListCategories;
+using JG.Code.Catalog.Domain.Entity;
 using JG.Code.Catalog.Domain.Repository;
+using JG.Code.Catalog.Domain.SeedWork.SearchableRepository;
 using JG.Code.Catalog.UnitTests.Common;
 using Moq;
 
@@ -40,6 +42,18 @@ public class ListCategoriesTestFixture : BaseFixture
         for (int i = 0; i < length; i++)        
             categories.Add(GetExampleCategory());        
         return categories;
+    }
+
+    public ListCategoriesInput GetExampleInput()
+    {
+        var random = new Random();
+        return new ListCategoriesInput(
+            page: random.Next(1, 10),
+            perPage: random.Next(15, 100),
+            search: Faker.Commerce.ProductName(),
+            sort: Faker.Commerce.ProductName(),
+            dir: random.Next(0, 10) > 5 ? SearchOrder.Asc : SearchOrder.Desc
+            );
     }
 
     public Mock<ICategoryRepository> GetRepositoryMock() => new();
