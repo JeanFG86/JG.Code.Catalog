0369e3df45f4a8ea3f9daed5c370bcd27c70f83d
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-07-13 16:24:36 -0300

    refactor DeleteCategoryTestFixture

--- tests/JG.Code.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTestFixture.cs
+++ tests/JG.Code.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -1,6 +1,7 @@
 ﻿using JG.Code.Catalog.Application.Interfaces;
 using JG.Code.Catalog.Domain.Entity;
 using JG.Code.Catalog.Domain.Repository;
+using JG.Code.Catalog.UnitTests.Application.Common;
 using JG.Code.Catalog.UnitTests.Common;
 using Moq;
 
@@ -9,30 +10,8 @@ namespace JG.Code.Catalog.UnitTests.Application.DeleteCategory;
 [CollectionDefinition(nameof(DeleteCategoryTestFixture))]
 public class GetCategoryFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture> { }
 
-public class DeleteCategoryTestFixture : BaseFixture
+public class DeleteCategoryTestFixture : CategoryUseCasesBaseFixture
 {
-    public string GetValidCategoryName()
-    {
-        var categoryName = "";
-        while (categoryName.Length < 3)
-            categoryName = Faker.Commerce.Categories(1)[0];
-        if (categoryName.Length > 255)
-            categoryName = categoryName[..255];
-        return categoryName;
-    }
-
-    public string GetValidCategoryDescription()
-    {
-        var categoryDescription = "";
-        if (categoryDescription.Length > 10_000)
-            categoryDescription = categoryDescription[..10_000];
-        return categoryDescription;
-    }
-
     public Category GetValidCategory()
         => new(GetValidCategoryName(), GetValidCategoryDescription());
-
-    public Mock<ICategoryRepository> GetRepositoryMock() => new();
-
-    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
 }
