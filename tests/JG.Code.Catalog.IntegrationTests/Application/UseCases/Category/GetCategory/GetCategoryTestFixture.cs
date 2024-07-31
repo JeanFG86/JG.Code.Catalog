190d9e2fe083cbea2404b121e1ba9425d167b12d
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-07-31 18:55:16 -0300

    GetCategory test

---
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using JG.Code.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+
+namespace JG.Code.Catalog.IntegrationTests.Application.UseCases.Category.GetCategory;
+
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureCollection : ICollectionFixture<GetCategoryTestFixture> { }
+
+public class GetCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+
+}
