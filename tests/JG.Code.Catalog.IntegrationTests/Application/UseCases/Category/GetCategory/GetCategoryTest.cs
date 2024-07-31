2dfd6ec3a356b28d7259de455dd8cdbf65a67dbb
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-07-31 18:56:55 -0300

    NotFoundExceptionWhenCategoryDoesntExist test

--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTest.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using JG.Code.Catalog.Application.Exceptions;
 using JG.Code.Catalog.Application.UseCases.Category.GetCategory;
 using JG.Code.Catalog.Infra.Data.EF.Repositories;
 using UseCase = JG.Code.Catalog.Application.UseCases.Category.GetCategory;
@@ -35,5 +36,22 @@ public class GetCategoryTest
         output.IsActive.Should().Be(exampleCategory.IsActive);
         output.Id.Should().Be(exampleCategory.Id);
         output.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+    }
+
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExist))]
+    [Trait("Integration/Application", "GetCategory - Use Cases")]
+    public async Task NotFoundExceptionWhenCategoryDoesntExist()
+    {
+        var exampleCategory = _fixture.GetExampleCategory();
+        var dbContext = _fixture.CreateDbContext();
+        dbContext.Categories.Add(exampleCategory);
+        dbContext.SaveChanges();
+        var repository = new CategoryRepository(dbContext);
+        var input = new UseCase.GetCategoryInput(Guid.NewGuid());
+        var useCase = new UseCase.GetCategory(repository);
+
+        var task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<NotFoundException>();
     }
 }
