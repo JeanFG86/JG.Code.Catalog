4654ed423730a006690e1957c7b74016689f4dc8
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-08-01 19:01:10 -0300

    UpdateCategoryOnlyName test

--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTest.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTest.cs
@@ -82,6 +82,38 @@ public class UpdateCategoryTest
         output.Name.Should().Be(input.Name);
         output.Description.Should().Be(input.Description);
         output.IsActive.Should().Be(exampleCategory.IsActive);
+    }
 
+    [Theory(DisplayName = nameof(UpdateCategoryOnlyName))]
+    [Trait("Integration/Application", "UpdateCategory - Use Cases")]
+    [MemberData(
+        nameof(UpdateCategoryDataGenerator.GetCategoriesToUpdate),
+        parameters: 5,
+        MemberType = typeof(UpdateCategoryDataGenerator)
+    )]
+    public async Task UpdateCategoryOnlyName(DomainEntity.Category exampleCategory, UpdateCategoryInput exampleInput)
+    {
+        var input = new UpdateCategoryInput(exampleInput.Id, exampleInput.Name);
+        var dbContext = _fixture.CreateDbContext();
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        await dbContext.AddRangeAsync(_fixture.GetExampleCategoriesList());
+        var trackingInfo = await dbContext.AddAsync(exampleCategory);
+        dbContext.SaveChanges();
+        trackingInfo.State = EntityState.Detached;
+        var useCase = new ApplicationUseCase.UpdateCategory(repository, unitOfWork);
+
+        CategoryModelOutput output = await useCase.Handle(input, CancellationToken.None);
+
+        var dbCategory = await (_fixture.CreateDbContext(true)).Categories.FindAsync(output.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(exampleCategory.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive);
+        dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(exampleCategory.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive);
     }
 }
