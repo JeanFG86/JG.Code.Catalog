4a54ee4d6ae72e70fb668e583e1d53a26b02464a
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-08-02 19:04:34 -0300

    SearchReturnsPaginated test

--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTest.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTest.cs
@@ -66,4 +66,44 @@ public class ListCategoriesTest
         output.Total.Should().Be(0);
         output.Items.Should().HaveCount(0);        
     }
+
+    [Theory(DisplayName = nameof(SearchReturnsPaginated))]
+    [Trait("Integration/Application", "ListCategories - Use Cases")]
+    [InlineData(10, 1, 5, 5)]
+    [InlineData(10, 2, 5, 5)]
+    [InlineData(7, 2, 5, 2)]
+    [InlineData(7, 3, 5, 0)]
+    public async Task SearchReturnsPaginated(
+       int quantityCategoriesToGenerate,
+       int page,
+       int perPage,
+       int expectedQuantityItems
+    )
+    {
+        CodeCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(quantityCategoriesToGenerate);
+        await dbContext.AddRangeAsync(exampleCategoriesList);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+        var categoryRepository = new CategoryRepository(dbContext);
+        var input = new ListCategoriesInput(page, perPage);
+        var useCase = new AppUseCases.ListCategories(categoryRepository);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Items.Should().NotBeNull();
+        output.Page.Should().Be(input.Page);
+        output.PerPage.Should().Be(input.PerPage);
+        output.Total.Should().Be(exampleCategoriesList.Count);
+        output.Items.Should().HaveCount(expectedQuantityItems);
+        foreach (CategoryModelOutput outputIem in output.Items)
+        {
+            var exampleItem = exampleCategoriesList.Find(category => category.Id == outputIem.Id);
+            exampleItem.Should().NotBeNull();
+            outputIem!.Name.Should().Be(exampleItem!.Name);
+            outputIem.Description.Should().Be(exampleItem.Description);
+            outputIem.IsActive.Should().Be(exampleItem.IsActive);
+            outputIem.CreatedAt.Should().Be(exampleItem.CreatedAt);
+        }
+    }
 }
