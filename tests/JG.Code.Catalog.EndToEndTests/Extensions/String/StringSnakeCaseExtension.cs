e3d95e01a32924fea2ad9ac54085705fd9b33de7
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-08-27 18:48:54 -0300

    create StringSnakeCaseExtension

---
+++ tests/JG.Code.Catalog.EndToEndTests/Extensions/String/StringSnakeCaseExtension.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace JG.Code.Catalog.EndToEndTests.Extensions.String;
+public static class StringSnakeCaseExtension
+{
+    private readonly static NamingStrategy _snakeCaseNamingStrategy = new SnakeCaseNamingStrategy();
+
+    public static string ToSnakeCase(this string value)
+    {
+        ArgumentNullException.ThrowIfNull(value, nameof(value));
+        return _snakeCaseNamingStrategy.GetPropertyName(value, false);
+    }
+}
