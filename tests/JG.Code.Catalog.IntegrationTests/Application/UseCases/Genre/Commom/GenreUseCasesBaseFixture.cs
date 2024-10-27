02d40b5774044484f9286bbe3bc7f7d5bbda8d8f
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-10-27 15:41:25 -0300

    GetGenreWithCategoryRelations test

--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Genre/Commom/GenreUseCasesBaseFixture.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Genre/Commom/GenreUseCasesBaseFixture.cs
@@ -20,4 +20,28 @@ public class GenreUseCasesBaseFixture : BaseFixture
     }
 
     public bool GetRandomBoolean() => new Random().NextDouble() < 0.5;
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = "";
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public DomainEntity.Category GetExampleCategory()
+        => new(GetValidCategoryName(), GetValidCategoryDescription(), GetRandomBoolean());
+
+    public List<DomainEntity.Category> GetExampleCategoriesList(int length = 10)
+        => Enumerable.Range(1, length).Select(_ => GetExampleCategory()).ToList();
 }
--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using JG.Code.Catalog.Application.Exceptions;
+using JG.Code.Catalog.Infra.Data.EF.Models;
 using JG.Code.Catalog.Infra.Data.EF.Repositories;
 using UseCase = JG.Code.Catalog.Application.UseCases.Genre.GetGenre;
 
@@ -54,5 +55,34 @@ public class GetGenreTest
         var action = async () =>  await useCase.Handle(input, CancellationToken.None);
 
         await action.Should().ThrowAsync<NotFoundException>().WithMessage($"Genre {randomGuid} not found.");
+    }
+
+
+    [Fact(DisplayName = nameof(GetGenreWithCategoryRelations))]
+    [Trait("Integration/Application", "GetGenre - Use Cases")]
+    public async Task GetGenreWithCategoryRelations()
+    {
+        var genresExampleList = _fixture.GetExampleListGenres();
+        var categoriesExampleList = _fixture.GetExampleCategoriesList(5);
+        var expectedGenre = genresExampleList[5];
+        categoriesExampleList.ForEach(category => expectedGenre.AddCategory(category.Id));
+        var dbArragneContext = _fixture.CreateDbContext();
+        await dbArragneContext.Categories.AddRangeAsync(categoriesExampleList);
+        await dbArragneContext.Genres.AddRangeAsync(genresExampleList);
+        await dbArragneContext.GenresCategories.AddRangeAsync(expectedGenre.Categories.Select(categoryId => new GenresCategories(categoryId, expectedGenre.Id)));
+        await dbArragneContext.SaveChangesAsync();
+        var genreRepository = new GenreRepository(_fixture.CreateDbContext(true));
+        var useCase = new UseCase.GetGenre(genreRepository);
+        var input = new UseCase.GetGenreInput(expectedGenre.Id);
+
+        var outPut = await useCase.Handle(input, CancellationToken.None);
+
+        outPut.Should().NotBeNull();
+        outPut.Id.Should().Be(expectedGenre.Id);
+        outPut.Name.Should().Be(expectedGenre.Name);
+        outPut.IsActive.Should().Be(expectedGenre.IsActive);
+        outPut.CreatedAt.Should().Be(expectedGenre.CreatedAt);
+        outPut.Categories.Should().HaveCount(expectedGenre.Categories.Count);
+        outPut.Categories.ToList().ForEach(id => expectedGenre.Categories.Should().Contain(id));
     }
 }
