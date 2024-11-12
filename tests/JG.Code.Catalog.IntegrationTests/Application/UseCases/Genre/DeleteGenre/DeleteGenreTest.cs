5ab06d92fa22a563fc53a71fc73355d0d4eb2b5b
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-11-11 22:31:20 -0300

    DeleteGenreThrowsWhenNotFound test

--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Genre/DeleteGenre/DeleteGenreTest.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -3,6 +3,7 @@ using JG.Code.Catalog.Infra.Data.EF.Repositories;
 using UseCase = JG.Code.Catalog.Application.UseCases.Genre.DeleteGenre;
 using Microsoft.EntityFrameworkCore;
 using JG.Code.Catalog.Infra.Data.EF;
+using JG.Code.Catalog.Application.Exceptions;
 
 namespace JG.Code.Catalog.IntegrationTests.Application.UseCases.Genre.DeleteGenre;
 
@@ -34,5 +35,24 @@ public  class DeleteGenreTest
         var assertDbContext = _fixture.CreateDbContext(true);
         var genreFromDb = await assertDbContext.Genres.FindAsync(targetGenre.Id);
         genreFromDb.Should().BeNull();
+    }
+
+    [Fact(DisplayName = nameof(DeleteGenreThrowsWhenNotFound))]
+    [Trait("Integration/Application", "DeleteGenre - Use Cases")]
+    public async Task DeleteGenreThrowsWhenNotFound()
+    {
+        var genresExampleList = _fixture.GetExampleListGenres();
+        var targetGenre = genresExampleList[5];
+        var dbArragneContext = _fixture.CreateDbContext();
+        await dbArragneContext.Genres.AddRangeAsync(genresExampleList);
+        await dbArragneContext.SaveChangesAsync();
+        var actDbContext = _fixture.CreateDbContext(true);
+        var useCase = new UseCase.DeleteGenre(new GenreRepository(actDbContext), new UnitOfWork(actDbContext));
+        var randomGuid = Guid.NewGuid();
+        var input = new UseCase.DeleteGenreInput(randomGuid);
+
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await action.Should().ThrowAsync<NotFoundException>().WithMessage($"Genre {randomGuid} not found.");
     }
 }
