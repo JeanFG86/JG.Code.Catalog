5f43e1f68f46bb3f391677a8a3334605bb479121
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-11-15 14:33:32 -0300

    ListGenres test

---
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Genre/ListGenres/ListGenresTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using JG.Code.Catalog.Application.UseCases.Genre.ListGenres;
+using JG.Code.Catalog.Infra.Data.EF.Repositories;
+using DomainEntity = JG.Code.Catalog.Domain.Entity;
+using UseCase = JG.Code.Catalog.Application.UseCases.Genre.ListGenres;
+
+namespace JG.Code.Catalog.IntegrationTests.Application.UseCases.Genre.ListGenres;
+
+[Collection(nameof(ListGenresTestFixture))]
+public class ListGenresTest
+{
+    private readonly ListGenresTestFixture _fixture;
+
+    public ListGenresTest(ListGenresTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(ListGenres))]
+    [Trait("Integration/Application", "ListGenres - Use Cases")]
+    public async Task ListGenres()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        var arrangeDbContext = _fixture.CreateDbContext();
+        await arrangeDbContext.AddRangeAsync(exampleGenres);
+        await arrangeDbContext.SaveChangesAsync();
+        UseCase.ListGenres useCase = new UseCase.ListGenres(new GenreRepository(_fixture.CreateDbContext(true)));
+        ListGenresInput input = new ListGenresInput(1, 20);
+
+        ListGenresOutput output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Page.Should().Be(input.Page);
+        output.PerPage.Should().Be(input.PerPage);
+        output.Items.Should().HaveCount(exampleGenres.Count);
+        output.Items.ToList().ForEach(outputItem =>
+        {
+            DomainEntity.Genre? exampleItem = exampleGenres.Find(example => example.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(exampleItem!.Name);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+        });
+    }
+}
---
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/Genre/ListGenres/ListGenresTestFixture.cs
@@ -0,0 +1,9 @@
+﻿using JG.Code.Catalog.IntegrationTests.Application.UseCases.Genre.Commom;
+
+namespace JG.Code.Catalog.IntegrationTests.Application.UseCases.Genre.ListGenres;
+
+[CollectionDefinition(nameof(ListGenresTestFixture))]
+public class ListGenresTestFixtureCollection : ICollectionFixture<ListGenresTestFixture> { }
+public class ListGenresTestFixture : GenreUseCasesBaseFixture
+{
+}
