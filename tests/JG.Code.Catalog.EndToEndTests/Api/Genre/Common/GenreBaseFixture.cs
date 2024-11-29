20e38bf929464fb95fdbdd725a65325f022ce7ef
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-11-28 21:32:08 -0300

    Create GenreBaseFixture & GenrePersistence

---
+++ tests/JG.Code.Catalog.EndToEndTests/Api/Genre/Common/GenreBaseFixture.cs
@@ -0,0 +1,13 @@
+﻿using JG.Code.Catalog.EndToEndTests.Common;
+
+namespace JG.Code.Catalog.EndToEndTests.Api.Genre.Common;
+
+public class GenreBaseFixture : BaseFixture
+{
+    public GenrePersistence Persistence { get; set; }
+
+    public GenreBaseFixture() : base()
+    {
+        Persistence = new GenrePersistence(CreateDbContext());
+    }
+}
---
+++ tests/JG.Code.Catalog.EndToEndTests/Api/Genre/Common/GenrePersistence.cs
@@ -0,0 +1,13 @@
+﻿using JG.Code.Catalog.Infra.Data.EF;
+
+namespace JG.Code.Catalog.EndToEndTests.Api.Genre.Common;
+
+public class GenrePersistence
+{
+    private readonly CodeCatalogDbContext _context;
+
+    public GenrePersistence(CodeCatalogDbContext context)
+    {
+        _context = context;
+    }
+}
