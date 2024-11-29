dec555271e59aeb2fe0ba219fd478402b40cb554
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-11-28 21:38:16 -0300

    Create GetGenreApiTestFixture

---
+++ tests/JG.Code.Catalog.EndToEndTests/Api/Genre/GetGenre/GetGenreApiTest.cs
@@ -0,0 +1,12 @@
+﻿namespace JG.Code.Catalog.EndToEndTests.Api.Genre.GetGenre;
+
+[Collection(nameof(GetGenreApiTestFixture))]
+public class GetGenreApiTest
+{
+    private readonly GetGenreApiTestFixture _fixture;
+
+    public GetGenreApiTest(GetGenreApiTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
---
+++ tests/JG.Code.Catalog.EndToEndTests/Api/Genre/GetGenre/GetGenreApiTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using JG.Code.Catalog.EndToEndTests.Api.Genre.Common;
+
+namespace JG.Code.Catalog.EndToEndTests.Api.Genre.GetGenre;
+
+[CollectionDefinition(nameof(GetGenreApiTestFixture))]
+public class GetGenreApiTestFixtureCollection : ICollectionFixture<GetGenreApiTestFixture>
+{ }
+public class GetGenreApiTestFixture : GenreBaseFixture
+{
+    
+}
