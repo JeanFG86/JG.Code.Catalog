5f0f5ca912ccd803a18bd1d2c49a5e4af56b9df5
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-12-01 15:11:22 -0300

    NotFound test

--- tests/JG.Code.Catalog.EndToEndTests/Api/Genre/GetGenre/GetGenreApiTest.cs
+++ tests/JG.Code.Catalog.EndToEndTests/Api/Genre/GetGenre/GetGenreApiTest.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using JG.Code.Catalog.Api.ApiModels.Response;
 using JG.Code.Catalog.Application.UseCases.Genre.Common;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JG.Code.Catalog.EndToEndTests.Api.Genre.GetGenre;
 using DomainEntity = Domain.Entity;
@@ -33,5 +34,22 @@ public class GetGenreApiTest
         output!.Data.Id.Should().Be(targetGenre.Id);
         output.Data.Name.Should().Be(targetGenre.Name);
         output.Data.IsActive.Should().Be(targetGenre.IsActive);
+    }
+    
+    [Fact(DisplayName = nameof(NotFound))]
+    [Trait("EndToEnd/API", "Genre/Get - Endpoints")]
+    public async Task NotFound()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        var randomGuid = Guid.NewGuid();
+        await _fixture.Persistence.InsertList(exampleGenres);
+
+        var (response, output) = await _fixture.ApiClient.Get<ProblemDetails>($"/genres/{randomGuid}");
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status404NotFound);
+        output.Should().NotBeNull();
+        output!.Type.Should().Be("NotFound");
+        output.Detail.Should().Be($"Genre {randomGuid} not found.");
     }
 }
