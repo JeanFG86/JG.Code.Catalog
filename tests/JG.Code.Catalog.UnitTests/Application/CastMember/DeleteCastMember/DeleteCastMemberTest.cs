07ff7b9fa6512de8943efd283584cba34ee6f366
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-01-30 20:29:00 -0300

    ThrowWhenCastMemberNotFound test

--- tests/JG.Code.Catalog.UnitTests/Application/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
+++ tests/JG.Code.Catalog.UnitTests/Application/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
@@ -1,6 +1,9 @@
-﻿using JG.Code.Catalog.Application.Interfaces;
+﻿using FluentAssertions;
+using JG.Code.Catalog.Application.Exceptions;
+using JG.Code.Catalog.Application.Interfaces;
 using JG.Code.Catalog.Domain.Repository;
 using UsesCases = JG.Code.Catalog.Application.UseCases.CastMember.DeleteCastMember;
+using DomainEntity = JG.Code.Catalog.Domain.Entity;
 using Moq;
 
 namespace JG.Code.Catalog.UnitTests.Application.CastMember.DeleteCastMember;
@@ -31,5 +34,24 @@ public class DeleteCastMemberTest
         repositoryMock.Verify(repository => repository.Get(castMemberExample.Id, It.IsAny<CancellationToken>()), Times.Once);
         repositoryMock.Verify(repository => repository.Delete(castMemberExample, It.IsAny<CancellationToken>()), Times.Once);
         unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    }
+    
+    [Fact(DisplayName = nameof(ThrowWhenCastMemberNotFound))]
+    [Trait("Application", "DeleteCastMember - Use Cases")]
+    public async Task ThrowWhenCastMemberNotFound()
+    {
+        var repositoryMock = new Mock<ICastMemberRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var exampleGuid = Guid.NewGuid();
+        repositoryMock.Setup(x => x.Get(exampleGuid, It.IsAny<CancellationToken>())).ThrowsAsync(new NotFoundException($"Cast member '{exampleGuid}' not found"));
+        var input = new UsesCases.DeleteCastMemberInput(exampleGuid);
+        var useCase = new UsesCases.DeleteCastMember(repositoryMock.Object, unitOfWorkMock.Object);
+
+        var task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<NotFoundException>();
+        repositoryMock.Verify(repository => repository.Get(exampleGuid, It.IsAny<CancellationToken>()), Times.Once);
+        repositoryMock.Verify(repository => repository.Delete(It.IsAny<DomainEntity.CastMember>(), It.IsAny<CancellationToken>()), Times.Never);
+        unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Never);
     }
 }
