0c0940eacb4a151285f451328a7ba80894c7757e
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-02-14 21:23:03 -0300

    ListOkWhenEmpty

--- tests/JG.Code.Catalog.UnitTests/Application/CastMember/ListCastMember/ListCastMemberTest.cs
+++ tests/JG.Code.Catalog.UnitTests/Application/CastMember/ListCastMember/ListCastMemberTest.cs
@@ -64,4 +64,43 @@ public class ListCastMemberTest
                 && searchInput.Order == input.Dir),
             It.IsAny<CancellationToken>()), Times.Once);
     }
+    
+    [Fact(DisplayName = nameof(ListOkWhenEmpty))]
+    [Trait("Application", "ListCastMembers - Use Cases")]
+    public async Task ListOkWhenEmpty()
+    {
+        var repositoryMock = new Mock<ICastMemberRepository>();
+        var input = _fixture.GetExampleInput();
+        var outputRepositorySearch = new SearchOutput<DomainEntity.CastMember>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            items: new List<DomainEntity.CastMember>().AsReadOnly(),
+            total: 0
+        );
+        repositoryMock.Setup(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput => searchInput.Page == input.Page
+                               && searchInput.PerPage == input.PerPage
+                               && searchInput.Search == input.Search
+                               && searchInput.OrderBy == input.Sort
+                               && searchInput.Order == input.Dir),
+            It.IsAny<CancellationToken>())).ReturnsAsync(outputRepositorySearch);
+        var useCase = new UseCase.ListCastMembers(repositoryMock.Object);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Page.Should().Be(outputRepositorySearch.CurrentPage);
+        output.PerPage.Should().Be(outputRepositorySearch.PerPage);
+        output.Total.Should().Be(0);
+        output.Items.Should().HaveCount(0);
+        repositoryMock.Verify(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput => searchInput.Page == input.Page
+                               && searchInput.PerPage == input.PerPage
+                               && searchInput.Search == input.Search
+                               && searchInput.OrderBy == input.Sort
+                               && searchInput.Order == input.Dir),
+            It.IsAny<CancellationToken>()), Times.Once);
+    }
 }
