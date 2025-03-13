e7a242c3510ed0e06714e81bc1068b7f6f0dccb7
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-03-13 20:07:09 -0300

    ThrowsWhenInvalidName test

--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/CastMember/CreateCastMember/CreateCastMemberTest.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/CastMember/CreateCastMember/CreateCastMemberTest.cs
@@ -1,6 +1,9 @@
 ﻿using FluentAssertions;
+using JG.Code.Catalog.Application.UseCases.Category.CreateCategory;
+using JG.Code.Catalog.Domain.Exceptions;
 using JG.Code.Catalog.Infra.Data.EF;
 using JG.Code.Catalog.Infra.Data.EF.Repositories;
+using Microsoft.EntityFrameworkCore;
 using UsesCases = JG.Code.Catalog.Application.UseCases.CastMember.CreateCastMember;
 
 namespace JG.Code.Catalog.IntegrationTests.Application.UseCases.CastMember.CreateCastMember;
@@ -37,5 +40,25 @@ public class CreateCastMemberTest
         output.Type.Should().Be(input.Type);
         output.Id.Should().NotBeEmpty();
         output.CreatedAt.Should().NotBeSameDateAs(default);
+    }
+    
+    [Theory(DisplayName = nameof(ThrowsWhenInvalidName))]
+    [Trait("Integration/Application", "CreateCastMember - Use Cases")]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public async Task ThrowsWhenInvalidName(string? name)
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var repository = new CastMemberRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var useCase = new UsesCases.CreateCastMember(repository, unitOfWork);
+        var exampleInput = new UsesCases.CreateCastMemberInput(name, _fixture.GetRandomCastMemberType());
+
+        var task = async () => await useCase.Handle(exampleInput, CancellationToken.None);
+
+        await task.Should().ThrowAsync<EntityValidationException>().WithMessage("Name should not be empty or null");
+        var dbCategoriesList = _fixture.CreateDbContext().Categories.AsNoTracking().ToList();
+        dbCategoriesList.Should().HaveCount(0);
     }
 }
