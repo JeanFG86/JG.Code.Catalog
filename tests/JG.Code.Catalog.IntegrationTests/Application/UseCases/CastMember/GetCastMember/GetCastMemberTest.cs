429cb06979b7ad7827d73d1a0ce2a1a9b3b4db2d
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-03-15 20:39:07 -0300

    GetCastMember test

---
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/CastMember/GetCastMember/GetCastMemberTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using JG.Code.Catalog.Infra.Data.EF.Repositories;
+using UseCase = JG.Code.Catalog.Application.UseCases.CastMember.GetCastMember;
+
+namespace JG.Code.Catalog.IntegrationTests.Application.UseCases.CastMember.GetCastMember;
+
+[Collection(nameof(GetCastMemberTestFixture))]
+public class GetCastMemberTest
+{
+    private readonly GetCastMemberTestFixture _fixture;
+
+    public GetCastMemberTest(GetCastMemberTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    [Fact(DisplayName = nameof(GetCastMember))]
+    [Trait("Integration/Application", "GetCastMember - Use Cases")]
+    public async Task GetCastMember()
+    {
+        var exampleCastMember = _fixture.GetExampleCastMember();
+        var dbContext = _fixture.CreateDbContext();
+        dbContext.CastMembers.Add(exampleCastMember);
+        await dbContext.SaveChangesAsync();
+        var repository = new CastMemberRepository(dbContext);                
+        var input = new UseCase.GetCastMemberInput(exampleCastMember.Id);
+        var useCase = new UseCase.GetCastMember(repository);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(exampleCastMember.Name);
+        output.Id.Should().Be(exampleCastMember.Id);
+        output.CreatedAt.Should().Be(exampleCastMember.CreatedAt);
+    }
+}
---
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/CastMember/GetCastMember/GetCastMemberTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using JG.Code.Catalog.IntegrationTests.Application.UseCases.CastMember.Common;
+
+namespace JG.Code.Catalog.IntegrationTests.Application.UseCases.CastMember.GetCastMember;
+
+[CollectionDefinition(nameof(GetCastMemberTestFixture))]
+public class GetCastMemberTestFixtureCollection : ICollectionFixture<GetCastMemberTestFixture>
+{
+}
+
+public class GetCastMemberTestFixture: CastMemberUseCasesBaseFixture
+{
+    
+}
