87c5abafd7bb3a0ded515b436a20debc203b49e2
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-03-15 20:45:22 -0300

    NotFoundExceptionWhenCastMemberDoesntExist test

--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/CastMember/GetCastMember/GetCastMemberTest.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/CastMember/GetCastMember/GetCastMemberTest.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using JG.Code.Catalog.Application.Exceptions;
 using JG.Code.Catalog.Infra.Data.EF.Repositories;
 using UseCase = JG.Code.Catalog.Application.UseCases.CastMember.GetCastMember;
 
@@ -32,5 +33,22 @@ public class GetCastMemberTest
         output.Name.Should().Be(exampleCastMember.Name);
         output.Id.Should().Be(exampleCastMember.Id);
         output.CreatedAt.Should().Be(exampleCastMember.CreatedAt);
+    }
+    
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCastMemberDoesntExist))]
+    [Trait("Integration/Application", "GetCastMember - Use Cases")]
+    public async Task NotFoundExceptionWhenCastMemberDoesntExist()
+    {
+        var exampleCastMember = _fixture.GetExampleCastMember();
+        var dbContext = _fixture.CreateDbContext();
+        dbContext.CastMembers.Add(exampleCastMember);
+        await dbContext.SaveChangesAsync();
+        var repository = new CastMemberRepository(dbContext);
+        var input = new UseCase.GetCastMemberInput(Guid.NewGuid());
+        var useCase = new UseCase.GetCastMember(repository);
+
+        var task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<NotFoundException>();
     }
 }
