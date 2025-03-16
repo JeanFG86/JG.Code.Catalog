54d8f69848f5c4f714a9ed18a66fac4e2d40c54b
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-03-16 20:16:23 -0300

    ThrowWhenCastMemberNotFound test

--- tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
+++ tests/JG.Code.Catalog.IntegrationTests/Application/UseCases/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using JG.Code.Catalog.Application.Exceptions;
 using JG.Code.Catalog.Application.UseCases.CastMember.DeleteCastMember;
 using JG.Code.Catalog.Infra.Data.EF;
 using JG.Code.Catalog.Infra.Data.EF.Repositories;
@@ -39,5 +40,23 @@ public class DeleteCastMemberTest
         dbCastMemberDeleted.Should().BeNull();
         var dbCastMembers = await assertDbContext.CastMembers.ToListAsync();
         dbCastMembers.Should().HaveCount(exampleList.Count);
+    }
+    
+    [Fact(DisplayName = nameof(ThrowWhenCastMemberNotFound))]
+    [Trait("Integration/Application", "DeleteCastMember - Use Cases")]
+    public async Task ThrowWhenCastMemberNotFound()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var exampleList = _fixture.GetExampleCastMembersList(10);
+        await dbContext.AddRangeAsync(exampleList);
+        await dbContext.SaveChangesAsync();
+        var repository = new CastMemberRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var useCase = new AppUseCase.DeleteCastMember(repository, unitOfWork);
+        var input = new DeleteCastMemberInput(Guid.NewGuid());
+
+        var task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<NotFoundException>().WithMessage($"CastMember '{input.Id}' not found.");
     }
 }
