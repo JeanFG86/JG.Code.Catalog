02b6166e2a9d37e08ffbed201c60eb19be11123b
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-03-24 22:20:13 -0300

    setup test api

---
+++ tests/JG.Code.Catalog.EndToEndTests/Api/CastMember/Common/CastMemberBaseFixture.cs
@@ -0,0 +1,16 @@
+﻿using JG.Code.Catalog.Domain.Enum;
+using JG.Code.Catalog.EndToEndTests.Common;
+
+namespace JG.Code.Catalog.EndToEndTests.Api.CastMember.Common;
+
+public class CastMemberBaseFixture : BaseFixture
+{
+    public string GetValidName() => Faker.Name.FullName();
+    public CastMemberType GetRandomCastMemberType() => (CastMemberType)(new Random()).Next(1,2);
+
+    public Domain.Entity.CastMember GetExampleCastMember()
+        => new(GetValidName(), GetRandomCastMemberType());
+    
+    public List<Domain.Entity.CastMember> GetExampleCastMembersList(int length = 10)
+        => Enumerable.Range(1, length).Select(_ => GetExampleCastMember()).ToList();
+}
---
+++ tests/JG.Code.Catalog.EndToEndTests/Api/CastMember/Common/CastMemberPersistence.cs
@@ -0,0 +1,23 @@
+﻿using JG.Code.Catalog.Infra.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using DomainEntity = JG.Code.Catalog.Domain.Entity;
+namespace JG.Code.Catalog.EndToEndTests.Api.CastMember.Common;
+
+public class CastMemberPersistence
+{
+    private readonly CodeCatalogDbContext _context;
+
+    public CastMemberPersistence(CodeCatalogDbContext context) 
+    {
+        _context = context;
+    }
+    
+    public async Task InsertList(List<DomainEntity.CastMember> castMembers)
+    {
+        await _context.AddRangeAsync(castMembers);
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task<DomainEntity.CastMember?> GetById(Guid id) => await _context.CastMembers.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
+    
+}
