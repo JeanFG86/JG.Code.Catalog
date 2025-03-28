4c7be06cbb7c990f2ceeb336c7e7a2323186ba63
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-03-27 21:32:03 -0300

    ErrorWhenNotFound test

--- tests/JG.Code.Catalog.EndToEndTests/Api/CastMember/GetCastMember/GetCastMemberApiTest.cs
+++ tests/JG.Code.Catalog.EndToEndTests/Api/CastMember/GetCastMember/GetCastMemberApiTest.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using JG.Code.Catalog.Api.ApiModels.Response;
 using JG.Code.Catalog.Application.UseCases.CastMember.Common;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JG.Code.Catalog.EndToEndTests.Api.CastMember.GetCastMember;
 
@@ -32,6 +33,25 @@ public class GetCastMemberApiTest : IDisposable
         output!.Data.Should().NotBeNull();
         output.Data.Name.Should().Be(example.Name);
         output.Data.Type.Should().Be(example.Type);
+    }
+    
+    [Fact(DisplayName = nameof(ErrorWhenNotFound))]
+    [Trait("EndToEnd/API", "CastMember/Get - Endpoints")]
+    public async Task ErrorWhenNotFound()
+    {
+        var examples = _fixture.GetExampleCastMembersList(20);
+        await _fixture.Persistence.InsertList(examples);
+        var randonGuid = Guid.NewGuid();
+
+        var (response, output) = await _fixture.ApiClient.Get<ProblemDetails>($"/castmembers/{randonGuid}");
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status404NotFound);
+        output.Should().NotBeNull();
+        output!.Status.Should().Be((int)StatusCodes.Status404NotFound);
+        output.Title.Should().Be("Not Found");
+        output.Detail.Should().Be($"CastMember '{randonGuid}' not found.");
+        output.Type.Should().Be("NotFound");
     }
     
     public void Dispose()
