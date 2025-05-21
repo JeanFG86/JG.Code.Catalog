d2f0760a6844eabdc1ccfa503fdf20772576ecc1
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-05-21 19:49:55 -0300

    CreateVideoTest

---
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/CreateVideo/CreateVideoTest.cs
@@ -0,0 +1,12 @@
+﻿namespace JG.Code.Catalog.UnitTests.Application.Video.CreateVideo;
+
+[Collection(nameof(CreateVideoTestFixtureCollection))]
+public class CreateVideoTest
+{
+    private readonly CreateVideoTestFixture _fixture;
+    
+    public CreateVideoTest(CreateVideoTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
---
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/CreateVideo/CreateVideoTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using JG.Code.Catalog.UnitTests.Common.Fixtures;
+
+namespace JG.Code.Catalog.UnitTests.Application.Video.CreateVideo;
+
+[CollectionDefinition(nameof(CreateVideoTestFixture))]
+public class CreateVideoTestFixtureCollection : ICollectionFixture<CreateVideoTestFixture>{}
+
+public class CreateVideoTestFixture : VideoTestFixtureBase
+{
+    
+}
