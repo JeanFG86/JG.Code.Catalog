4d52a51f17d9d883a92f34b42f598b2d884fb363
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-07-09 20:53:10 -0300

    refactor

--- src/JG.Code.Catalog.Application/UseCases/Video/CreateVideo/CreateVideo.cs
+++ src/JG.Code.Catalog.Application/UseCases/Video/CreateVideo/CreateVideo.cs
@@ -32,39 +32,51 @@ public class CreateVideo : ICreateVideo
             throw new EntityValidationException("There are validation errors", validationHandler.Errors);
         if ((input.CategoriesIds?.Count ?? 0) > 0)
         {
-            var persistenceIds = await _categoryRepository.GetIdsListByIds(input.CategoriesIds!.ToList(), cancellationToken);
-            if (persistenceIds.Count < input.CategoriesIds!.Count)
-            {
-                var notFoundIds = input.CategoriesIds!.ToList().FindAll(categoryId => !persistenceIds.Contains(categoryId));
-                throw new RelatedAggregateException($"Related category id (or ids) not found: {string.Join(',', notFoundIds)}");
-            }
-            input.CategoriesIds!.ToList().ForEach(video.AddCategory);  
+            await ValidateCategoryIds(input, cancellationToken);
+            input.CategoriesIds!.ToList().ForEach(video.AddCategory);
         }
-
         if ((input.GenresIds?.Count ?? 0) > 0)
         {
-            var persistenceIds = await _genreRepository.GetIdsListByIds(input.GenresIds!.ToList(), cancellationToken);
-            if (persistenceIds.Count < input.GenresIds!.Count)
-            {
-                var notFoundIds = input.GenresIds!.ToList().FindAll(genreId => !persistenceIds.Contains(genreId));
-                throw new RelatedAggregateException($"Related genre id (or ids) not found: {string.Join(',', notFoundIds)}");
-            }
-            input.GenresIds!.ToList().ForEach(video.AddGenre);  
+            await ValidateAndRetrieveGenreIds(input, cancellationToken);
+            input.GenresIds!.ToList().ForEach(video.AddGenre);
         }
-
         if ((input.CastMembersIds?.Count ?? 0) > 0)
         {
-            var persistenceIds = await _castMemberRepository.GetIdsListByIds(input.CastMembersIds!.ToList(), cancellationToken);
-            if (persistenceIds.Count < input.CastMembersIds!.Count)
-            {
-                var notFoundIds = input.CastMembersIds!.ToList().FindAll(genreId => !persistenceIds.Contains(genreId));
-                throw new RelatedAggregateException($"Related cast member id (or ids) not found: {string.Join(',', notFoundIds)}");
-            }
+            await ValidateCastMemberIds(input, cancellationToken);
             input.CastMembersIds!.ToList().ForEach(video.AddCastMember);
         }
-        
         await _videoRepository.Insert(video, cancellationToken);
         await _unitOfWork.Commit(cancellationToken);
         return CreateVideoOutput.FromVideo(video);
+    }
+
+    private async Task ValidateCastMemberIds(CreateVideoInput input, CancellationToken cancellationToken)
+    {
+        var persistenceIds = await _castMemberRepository.GetIdsListByIds(input.CastMembersIds!.ToList(), cancellationToken);
+        if (persistenceIds.Count < input.CastMembersIds!.Count)
+        {
+            var notFoundIds = input.CastMembersIds!.ToList().FindAll(genreId => !persistenceIds.Contains(genreId));
+            throw new RelatedAggregateException($"Related cast member id (or ids) not found: {string.Join(',', notFoundIds)}");
+        }
+    }
+
+    private async Task ValidateAndRetrieveGenreIds(CreateVideoInput input, CancellationToken cancellationToken)
+    {
+        var persistenceIds = await _genreRepository.GetIdsListByIds(input.GenresIds!.ToList(), cancellationToken);
+        if (persistenceIds.Count < input.GenresIds!.Count)
+        {
+            var notFoundIds = input.GenresIds!.ToList().FindAll(genreId => !persistenceIds.Contains(genreId));
+            throw new RelatedAggregateException($"Related genre id (or ids) not found: {string.Join(',', notFoundIds)}");
+        }
+    }
+
+    private async Task ValidateCategoryIds(CreateVideoInput input, CancellationToken cancellationToken)
+    {
+        var persistenceIds = await _categoryRepository.GetIdsListByIds(input.CategoriesIds!.ToList(), cancellationToken);
+        if (persistenceIds.Count < input.CategoriesIds!.Count)
+        {
+            var notFoundIds = input.CategoriesIds!.ToList().FindAll(categoryId => !persistenceIds.Contains(categoryId));
+            throw new RelatedAggregateException($"Related category id (or ids) not found: {string.Join(',', notFoundIds)}");
+        }
     }
 }
