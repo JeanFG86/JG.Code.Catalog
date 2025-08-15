b3b394989c8ea02b53a35f5623fd5655a65063b8
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-08-14 21:48:34 -0300

    UploadMideasTest setup

---
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/UploadMideas/UploadMideasTest.cs
@@ -0,0 +1,19 @@
+﻿namespace JG.Code.Catalog.UnitTests.Application.Video.UploadMideas;
+
+[Collection(nameof(UploadMideasTestFixture))]
+public class UploadMideasTest
+{
+    private readonly UploadMideasTestFixture _fixture;
+    
+    public UploadMideasTest(UploadMideasTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    [Fact(DisplayName = nameof(UploadMideas))]
+    [Trait("Application ", "UploadMideas - Use Cases")]
+    public void UploadMideas()
+    {
+        
+    }
+}
---
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/UploadMideas/UploadMideasTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using JG.Code.Catalog.UnitTests.Common.Fixtures;
+
+namespace JG.Code.Catalog.UnitTests.Application.Video.UploadMideas;
+
+[CollectionDefinition(nameof(UploadMideasTestFixture))]
+public class UploadMideasTestFixtureFixtureCollection : ICollectionFixture<UploadMideasTestFixture>{}
+
+public class UploadMideasTestFixture : VideoTestFixtureBase
+{
+    
+}
