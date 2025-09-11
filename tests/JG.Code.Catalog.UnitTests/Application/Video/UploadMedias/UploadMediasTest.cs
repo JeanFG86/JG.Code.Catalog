9a578ce5e12abd06651b88c424568305163801f2
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-09-10 21:46:37 -0300

    Add ClearStorageInCommitErrorCase test to validate rollback on commit errors

--- tests/JG.Code.Catalog.UnitTests/Application/Video/UploadMedias/UploadMediasTest.cs
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/UploadMedias/UploadMediasTest.cs
@@ -83,4 +83,35 @@ public class UploadMediasTest
         _storageService.Verify(x => x.Upload(It.Is<string>(x => fileNames.Contains(x)), It.IsAny<Stream>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
         _storageService.Verify(x => x.Delete(It.Is<string>(x => x == videoStoragePath), It.IsAny<CancellationToken>()), Times.Once);
     }
+    
+    [Fact(DisplayName = nameof(ClearStorageInCommitErrorCase))]
+    [Trait("Application ", "UploadMedias - Use Cases")]
+    public async void ClearStorageInCommitErrorCase()
+    {
+        var video = _fixture.GetValidVideo();
+        var validInput = _fixture.GetValidInput(video.Id);
+        var videoFileName = StorageFileName.Create(video.Id, nameof(video.Media), validInput.VideoFile!.Extension);
+        var videoStoragePath = $"storage/{videoFileName}";
+        var trailerFileName = StorageFileName.Create(video.Id, nameof(video.Trailer), validInput.TrailerFile!.Extension);
+        var trailerStoragePath = $"storage/{trailerFileName}";
+        var fileNames = new List<string> { videoFileName, trailerFileName};
+        var filePathNames = new List<string> { videoStoragePath, trailerStoragePath};
+        _repositoryMock.Setup(x => x.Get(It.Is<Guid>(x => x == video.Id), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(video);
+        _storageService.Setup(x => x.Upload(It.Is<string>(x => x == videoFileName), It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(videoStoragePath);
+        _storageService.Setup(x => x.Upload(It.Is<string>(x => x == trailerFileName), It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(trailerStoragePath);
+        _unitOfWorkMock.Setup(x => x.Commit(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Something went wrong whit the commit"));
+        
+        
+        var action = () => _useCase.Handle(validInput, cancellationToken: CancellationToken.None);
+        
+        await action.Should().ThrowAsync<Exception>().WithMessage("Something went wrong whit the commit");
+        _repositoryMock.VerifyAll();
+        _storageService.Verify(x => x.Upload(It.Is<string>(x => fileNames.Contains(x)), It.IsAny<Stream>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+        _storageService.Verify(x => x.Delete(It.Is<string>(fileName => filePathNames.Contains(fileName)), It.IsAny<CancellationToken>()), Times.Exactly(2));
+        _storageService.Verify(x => x.Delete(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+    }
 }
