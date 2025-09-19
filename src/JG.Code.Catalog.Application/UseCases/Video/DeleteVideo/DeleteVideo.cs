dda7072facc4e7484d3e86ff3e73643ef172b856
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-09-18 21:40:28 -0300

    DeleteVideoWithAllMediasAndClearStorage

--- src/JG.Code.Catalog.Application/UseCases/Video/DeleteVideo/DeleteVideo.cs
+++ src/JG.Code.Catalog.Application/UseCases/Video/DeleteVideo/DeleteVideo.cs
@@ -7,11 +7,13 @@ public class DeleteVideo : IDeleteVideo
 {
     private readonly IVideoRepository _videoRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IStorageService _storageService;
 
-    public DeleteVideo(IVideoRepository videoRepository, IUnitOfWork unitOfWork)
+    public DeleteVideo(IVideoRepository videoRepository, IUnitOfWork unitOfWork, IStorageService storageService)
     {
         _videoRepository = videoRepository;
         _unitOfWork = unitOfWork;
+        _storageService = storageService;
     }
 
     public async Task Handle(DeleteVideoInput input, CancellationToken cancellationToken)
@@ -19,5 +21,9 @@ public class DeleteVideo : IDeleteVideo
         var video = await _videoRepository.Get(input.Id, cancellationToken);
         await _videoRepository.Delete(video, cancellationToken);
         await _unitOfWork.Commit(cancellationToken);
+        if(video.Trailer is not null)
+            await _storageService.Delete(video.Trailer.FilePath, cancellationToken);
+        if(video.Media is not null)
+            await _storageService.Delete(video.Media.FilePath, cancellationToken);
     }
 }
--- tests/JG.Code.Catalog.UnitTests/Application/Video/DeleteVideo/DeleteVideoTest.cs
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/DeleteVideo/DeleteVideoTest.cs
@@ -14,13 +14,15 @@ public class DeleteVideoTest
     private readonly UseCase.DeleteVideo _useCase;
     private readonly Mock<IVideoRepository> _repositoryMock;
     private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<IStorageService> _storageService;
 
     public DeleteVideoTest(DeleteVideoTestFixture fixture)
     {
         _fixture = fixture;
         _repositoryMock = new Mock<IVideoRepository>();
         _unitOfWorkMock = new Mock<IUnitOfWork>();
-        _useCase = new UseCase.DeleteVideo(_repositoryMock.Object, _unitOfWorkMock.Object);
+        _storageService = new Mock<IStorageService>();
+        _useCase = new UseCase.DeleteVideo(_repositoryMock.Object, _unitOfWorkMock.Object, _storageService.Object);
     }
 
     [Fact(DisplayName = nameof(DeleteVideo))]
@@ -37,5 +39,26 @@ public class DeleteVideoTest
         _repositoryMock.VerifyAll();
         _repositoryMock.Verify(x => x.Delete(It.Is<DomainEntity.Video>(x => x.Id == videoExample.Id), It.IsAny<CancellationToken>()), Times.Once);
         _unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact(DisplayName = nameof(DeleteVideoWithAllMediasAndClearStorage))]
+    [Trait("Application ", "DeleteVideo - Use Cases")]
+    public async Task DeleteVideoWithAllMediasAndClearStorage()
+    {
+        var videoExample = _fixture.GetValidVideo();
+        videoExample.UpdateMedia(_fixture.GetValidMediaPath());
+        videoExample.UpdateTrailer(_fixture.GetValidMediaPath());
+        var filePaths = new List<string>() { videoExample.Media!.FilePath, videoExample.Trailer!.FilePath };
+        var validInput = _fixture.GetValidInput(videoExample.Id);
+        _repositoryMock.Setup(x => x.Get(It.Is<Guid>(x => x == videoExample.Id), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(videoExample);
+
+        await _useCase.Handle(validInput, CancellationToken.None);
+
+        _repositoryMock.VerifyAll();
+        _repositoryMock.Verify(x => x.Delete(It.Is<DomainEntity.Video>(x => x.Id == videoExample.Id), It.IsAny<CancellationToken>()), Times.Once);
+        _unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
+        _storageService.Verify(x => x.Delete(It.Is<string>(filePath => filePaths.Contains(filePath)), It.IsAny<CancellationToken>()), Times.Exactly(2));
+        _storageService.Verify(x => x.Delete(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
     }
 }
