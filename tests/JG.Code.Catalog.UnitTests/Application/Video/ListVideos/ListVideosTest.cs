c3365753424ba1d7d696bce91a2dd283503ee73c
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-11-04 16:50:22 -0300

    ListVideosTest setup

---
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTest.cs
@@ -0,0 +1,12 @@
+﻿namespace JG.Code.Catalog.UnitTests.Application.Video.ListVideos;
+
+[Collection(nameof(ListVideosTestFixtureCollection))]
+public class ListVideosTest
+{
+    private readonly ListVideosTestFixture _fixture;
+
+    public ListVideosTest(ListVideosTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
---
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using JG.Code.Catalog.UnitTests.Common.Fixtures;
+
+namespace JG.Code.Catalog.UnitTests.Application.Video.ListVideos;
+
+[CollectionDefinition(nameof(ListVideosTestFixtureCollection))]
+public class ListVideosTestFixtureCollection : ICollectionFixture<ListVideosTestFixture>
+{
+}
+
+public class ListVideosTestFixture : VideoTestFixtureBase
+{
+}
