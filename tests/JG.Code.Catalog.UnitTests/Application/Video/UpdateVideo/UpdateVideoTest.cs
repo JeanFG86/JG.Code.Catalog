f5be95f6850684cff6ec40e2aa7350c0a8fe2b60
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2026-01-06 20:14:19 -0300

    UpdateVideoTest

---
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/UpdateVideo/UpdateVideoTest.cs
@@ -0,0 +1,12 @@
+﻿namespace JG.Code.Catalog.UnitTests.Application.Video.UpdateVideo;
+
+[Collection(nameof(UpdateVideoTestFixture))]
+public class UpdateVideoTest
+{
+    private readonly UpdateVideoTestFixture _fixture;
+
+    public UpdateVideoTest(UpdateVideoTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
---
+++ tests/JG.Code.Catalog.UnitTests/Application/Video/UpdateVideo/UpdateVideoTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using JG.Code.Catalog.UnitTests.Common.Fixtures;
+
+namespace JG.Code.Catalog.UnitTests.Application.Video.UpdateVideo;
+
+[CollectionDefinition(nameof(UpdateVideoTestFixture))]
+public class UpdateVideoTestFixtureCollection : ICollectionFixture<UpdateVideoTestFixture>
+{
+}
+
+public class UpdateVideoTestFixture : VideoTestFixtureBase
+{
+}
