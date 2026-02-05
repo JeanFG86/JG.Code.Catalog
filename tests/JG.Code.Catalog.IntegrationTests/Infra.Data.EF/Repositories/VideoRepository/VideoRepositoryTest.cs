da4bbfa3455676399459d1fb4f82afadb1f62630
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2026-02-05 20:35:44 -0300

    VideoRepositoryTestFixture & VideoRepositoryTest

---
+++ tests/JG.Code.Catalog.IntegrationTests/Infra.Data.EF/Repositories/VideoRepository/VideoRepositoryTest.cs
@@ -0,0 +1,19 @@
+﻿
+namespace JG.Code.Catalog.IntegrationTests.Infra.Data.EF.Repositories.VideoRepository;
+
+
+[Collection(nameof(VideoRepositoryTestFixture))]
+public class VideoRepositoryTest
+{
+    private readonly VideoRepositoryTestFixture _fixture;
+    public VideoRepositoryTest(VideoRepositoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "Video Repository - Repositories")]
+    public void Insert()
+    {
+    }
+}
---
+++ tests/JG.Code.Catalog.IntegrationTests/Infra.Data.EF/Repositories/VideoRepository/VideoRepositoryTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using JG.Code.Catalog.IntegrationTests.Common;
+
+namespace JG.Code.Catalog.IntegrationTests.Infra.Data.EF.Repositories.VideoRepository;
+
+[CollectionDefinition(nameof(VideoRepositoryTestFixture))]
+public class VideoRepositoryTestFixtureCollection : ICollectionFixture<VideoRepositoryTestFixture>
+{
+
+}
+
+public class VideoRepositoryTestFixture : BaseFixture
+{
+
+}
