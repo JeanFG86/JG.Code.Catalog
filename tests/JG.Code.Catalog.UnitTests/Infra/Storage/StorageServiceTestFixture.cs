11ab590ae6b0af59473748ee2bc2d881d3ba775f
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2026-07-07 21:12:02 -0300

    Add Infra.Storage project with GCS integration and tests
    Added JG.Code.Catalog.Infra.Storage targeting .NET 8.0 with Google Cloud Storage support. Updated solution and unit test project references. Introduced StorageServiceTest with Moq-based upload tests and a test fixture placeholder.

---
+++ tests/JG.Code.Catalog.UnitTests/Infra/Storage/StorageServiceTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG.Code.Catalog.UnitTests.Infra.Storage;
+
+internal class StorageServiceTestFixture
+{
+}
